ef57996027eba50e3cf814a4b2e220250a30f990
ben <types@ccs.neu.edu>
2015-04-28 14:12:32 -0400

    ** packaging 101

---
+++ info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+;; 2015-04-28:
+;; Not sure exactly what we need here, but these settings
+;; seem like a good guess
+
+(define deps '("base"))
+;(define build-deps '("scribble-lib" "racket-docs"))
+(define pkg-desc "Library for SICM")
+(define pkg-authors '(ben ben))
+(define version "0.0")
---
+++ main.rkt
@@ -0,0 +1,30 @@
+#lang racket/base
+
+;; 2015-04-27:
+;; Macros for the SICM library.
+;; Placed here (as 'main.rkt' in the top directory) for convenience developing.
+
+;; The contents of this file will eventually live somewhere else,
+;; and 'main.rkt' will just be the library's API.
+
+(provide
+ def
+ ;replace 'provide' with a form that requires a doc and a contract
+ ;macros for easy list access (to replace vector-ref, etc)
+)
+
+;; -----------------------------------------------------------------------------
+
+(require
+ (for-syntax
+  racket/base
+  syntax/parse))
+
+;; =============================================================================
+
+;; Sample macro, to make sure package install worked
+(define-syntax (def stx)
+  (syntax-parse stx
+    [(_ x:id e)
+     (printf "MACRO WORKED\n")
+     #'(define x e)]))
