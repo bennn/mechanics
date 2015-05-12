4775056b3f08845212333e97ecc42bf1f38621b6
ben <types@ccs.neu.edu>
2015-05-12 12:43:53 -0400

    [provide++] added provided/api macro

--- main.rkt
+++ main.rkt
@@ -8,23 +8,32 @@
 ;; and 'main.rkt' will just be the library's API.
 
 (provide
- def
- ;replace 'provide' with a form that requires a doc and a contract
- ;macros for easy list access (to replace vector-ref, etc)
+ provide/api
+ ;; (provide (ID CONTRACT DOC) ...)
+ ;; An optional `provide` form for identifiers that requires a contract and
+ ;;  docstring. Desugars to a `contract-out` for the identifier.
+ (all-from-out racket/contract)
+ ;; Re-export `racket/contract`, for `provide/api` to work.
 )
 
 ;; -----------------------------------------------------------------------------
 
 (require
+ racket/contract
  (for-syntax
   racket/base
-  syntax/parse))
+  syntax/parse
+  (only-in unstable/sequence in-syntax)))
 
 ;; =============================================================================
 
-;; Sample macro, to make sure package install worked
-(define-syntax (def stx)
+(define-syntax (provide/api stx)
   (syntax-parse stx
-    [(_ x:id e)
-     (printf "MACRO WORKED\n")
-     #'(define x e)]))
+    [(_ (~seq pr*:id
+              #:contract ctr*
+              #:doc      doc*:str) ...)
+     ;; Ignores the docstring for now.
+     ;;  should check if compiling in documentation mode & save doc.
+     ;; Also, a "no contract" form/flag would be nice.
+     #'(provide (contract-out [pr* ctr*] ...))]
+    [_ (error (format "provide/api: syntax error, expected '(provide/api (ID CONTRACT DOC) ...)' but given '~a'" (syntax->datum stx)))]))
