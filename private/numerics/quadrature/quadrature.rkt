e0a9e6bb0be01bf55b9fd80ef14b4d51345f86d2
ben <types@ccs.neu.edu>
2015-08-26 00:44:59 -0400

    [numerics/quadrature] refactored using match

--- private/numerics/quadrature/quadrature.rkt
+++ private/numerics/quadrature/quadrature.rkt
@@ -14,7 +14,7 @@
 ;; -----------------------------------------------------------------------------
 
 (require
-  (only-in racket/match match-define)
+  racket/match
 ;  (only-in "rational.rkt"
 ;    integrate-closed-closed
 ;    integrate-closed-closed-1
@@ -29,13 +29,15 @@
 
 ;; =============================================================================
 
+;; (define-type Method (U open closed-closed closed-open open-closed open-open romberg bulirsch-stoer)
+
 ;; TODO document
 (struct definite-integrator (
   integrand ;; (U #f TODO)
   lower-limit ;; (U #f TODO)
   upper-limit ;; (U #f TODO)
   error ;; Flonum
-  method ;; (U open TODO)
+  method ;; Method
 ) #:mutable ;; All fields can be mutated
   #:property prop:procedure ;; "Applying" an integrator evaluates it
   (lambda (self) (apply-definite-integrator self))
@@ -54,142 +56,116 @@
 ;; (-> definite-integrator TODO)
 (define (apply-definite-integrator di)
   (match-define (definite-integrator i ll ul e m) di)
-  (printf "evaluating integrand ~a with method ~a\n" i m))
-  ;(evaluate-definite-integral i #:lo ll #:hi ul #:method m #:epsilon e))
-
-(define di (make-definite-integrator))
-(set-definite-integrator-integrand! di 'yes)
-(di)
+  (evaluate-definite-integral i #:lo ll #:hi ul #:method m #:epsilon e))
 
 ;; -----------------------------------------------------------------------------
 
-;(define (evaluate-definite-integral integrand
-;                                    method
-;                                    lower-limit-exact
-;                                    upper-limit-exact
-;                                    allowable-error-exact)
-;  (define lo (exact->inexact lower-limit-exact))
-;  (define hi (exact->inexact upper-limit-exact))
-;  (define eps (exact->inexact allowable-error-exact))
-;  (cond
-;   [(inf? lo hi)
-;    ;;bg; why swap lo/hi order?
-;    (evaluate-improper-integral method integrand hi lo eps)]
-;   [else
-;    ;;bg; do we really need all these different functions?
-;    (case method
-;     [(open)
-;      (integrate-open integrand lo hi eps)]
-;     [(closed-closed)
-;      (integrate-closed-closed-1 integrand lo hi eps)]
-;     [(closed-open)
-;      (integrate-closed-open-1 integrand lo hi eps)]
-;     [(open-closed)
-;      (integrate-open-closed-1 integrand lo hi eps)]
-;     [(open-open)
-;      (integrate-open-open integrand lo hi eps)]
-;     [(romberg)
-;      (romberg-quadrature integrand lo hi eps)]
-;     [(bulirsch-stoer)
-;      (bulirsch-stoer-quadrature integrand lo hi eps)]
-;     [else
-;      (error 'evaluate-definite-integral (format "Unknown method '~a'" method))])]))
-;
-;;;bg; why hi/lo, instead of lo/hi?
-;(define (evaluate-improper-integral method integrand hi lo eps)
-;  (define new-integrand
-;    (lambda (theta) (/ (integrand (tan theta))
-;                       (square (cos theta)))))
-;  ;;bg; refactor into a match
-;  (case lo
-;   [(neg-inf)
-;    (case hi
-;     [(pos-inf)
-;      (integrate-open-open new-integrand -pi/2 +pi/2 eps)]
-;     [(neg-inf)
-;      0.0]
-;     [else
-;      (case method 
-;        [(open-open closed-open)
-;         (integrate-open-open new-integrand -pi/2 (atan hi) eps)]
-;        [else
-;         (integrate-open-closed new-integrand -pi/2 (atan hi) eps)])])]
-;  [(pos-inf)
-;   (case hi
-;    [(pos-inf)
-;     0.0]
-;    [(neg-inf)
-;     (- (integrate-open-open new-integrand -pi/2 +pi/2 eps))]
-;    [else
-;     (case method
-;      [(open-open open-closed)
-;       (- (integrate-open-open new-integrand (atan hi) -pi/2 eps))]
-;      [else
-;       (- (integrate-closed-open new-integrand (atan hi) +pi/2 eps))])])]
-;  [else
-;   (case hi
-;    [(pos-inf)
-;     (case method
-;      [(open-open open-closed)
-;       (integrate-open-open new-integrand (atan lo) +pi/2 eps)]
-;      [else
-;       (integrate-closed-open new-integrand (atan lo) +pi/2 eps)])]
-;    [(neg-inf)
-;     (case method
-;      [(open-open open-closed)
-;       (- integrate-open-open new-integrand -pi/2 (atan lo) eps)]
-;      [else
-;       (- (integrate-closed-open new-integrand -pi/2 (atan lo) eps))])])]))
-;
-;;; Simpler version from Press et~al.
-;(define *improper-integral-breakpoint* (make-parameter 1.0))
-;(define (evaluate-improper-integral/press method integrand hi lo eps)
-;  (define new-integrand
-;    (lambda (t)
-;      (/ (integrand (/ 1.0 t))
-;         (* t t))))
-;  (case lo
-;   [(neg-inf)
-;    (case hi
-;     [(neg-inf)
-;      0.0]
-;     [(pos-inf)
-;      (+ (integrate-closed-open new-integrand (/ -1.0 (*improper-integral-breakpoint*)) 0.0 eps)
-;         (integrate-closed-closed integrand (- (*improper-integral-breakpoint*)) (*improper-integral-breakpoint*) eps)
-;         (integrate-open-closed new-integrand 0.0 (/ 1.0 (*improper-integral-breakpoint*)) eps))]
-;    [else
-;     (if (<= hi (- (*improper-integral-breakpoint*)))
-;       (integrate-open-open new-integrand (/ -1.0 hi) 0.0 eps)
-;       (+ (integrate-closed-open new-integrand (/ -1.0 (*improper-integral-breakpoint*)) 0.0 eps)
-;          (integrate-closed-open integrand (- (*improper-integral-breakpoint*)) hi eps)))])]
-;   [(pos-inf)
-;    (case hi
-;     [(neg-inf)
-;      (- (+ (integrate-closed-open new-integrand (/ -1.0 (*improper-integral-breakpoint*)) 0.0 eps)
-;         (integrate-closed-closed integrand (- (*improper-integral-breakpoint*)) (*improper-integral-breakpoint*) eps)
-;         (integrate-open-closed new-integrand 0.0 (/ 1.0 (*improper-integral-breakpoint*)) eps)))]
-;     [(pos-inf)
-;      0.0]
-;     [else
-;      (if (>= hi (*improper-integral-breakpoint*))
-;        (- (integrate-open-open new-integrand (/ 1.0 hi) 0.0 eps))
-;        (- (+ (integrate-closed-open new-integrand (/ 1.0 (*improper-integral-breakpoint*)) 0.0 eps)
-;              (integrate-closed-open integrand (*improper-integral-breakpoint*) hi eps))))])]
-;   [else
-;    (case hi
-;     [(neg-inf)
-;      (if (<= lo (- (*improper-integral-breakpoint*)))
-;        (integrate-open-open new-integrand (/ -1.0 lo) 0.0 eps)
-;        (+ (integrate-closed-open new-integrand (/ -1.0 (*improper-integral-breakpoint*)) 0.0 eps)
-;           (integrate-closed-open integrand (*improper-integral-breakpoint*) lo eps)))]
-;     [(pos-inf)
-;      (if (>= lo (*improper-integral-breakpoint*))
-;        (integrate-open-open new-integrand 0.0 (/ 1.0 lo) eps)
-;        (+ (integrate-open-closed integrand lo (*improper-integral-breakpoint*) eps)
-;           (integrate-open-closed new-integrand 0.0 (/ 1.0 (*improper-integral-breakpoint*)) eps)))]
-;     [else
-;       (error 'quadrature:press "Should not get here")])]))
-;
+(define (evaluate-definite-integral integrand
+                                    #:method method
+                                    #:lo lower-limit-exact
+                                    #:hi upper-limit-exact
+                                    #:epsilon eps-exact)
+  (define lo (exact->inexact lower-limit-exact))
+  (define hi (exact->inexact upper-limit-exact))
+  (define eps (exact->inexact epsilon))
+  (if (or (inf? lo) (inf? hi))
+    (evaluate-improper-integral integrand method lo hi eps)
+    ;;bg; do we really need all these different functions?
+    (case method
+     [(open)
+      (integrate-open integrand lo hi eps)]
+     [(closed-closed)
+      (integrate-closed-closed-1 integrand lo hi eps)]
+     [(closed-open)
+      (integrate-closed-open-1 integrand lo hi eps)]
+     [(open-closed)
+      (integrate-open-closed-1 integrand lo hi eps)]
+     [(open-open)
+      (integrate-open-open integrand lo hi eps)]
+     [(romberg)
+      (romberg-quadrature integrand lo hi eps)]
+     [(bulirsch-stoer)
+      (bulirsch-stoer-quadrature integrand lo hi eps)]
+     [else
+      (error 'evaluate-definite-integral (format "Unknown method '~a'" method))])]))
+
+;; (-> Method Integrand TODO TODO Flonum Flonum)
+(define (evaluate-improper-integral method integrand lo hi eps)
+  (define (new-integrand theta)
+    (/ (integrand (tan theta))
+       (square (cos theta))))
+  (define oo/co? (or (eq? method 'open-open) (eq? method 'closed-open)))
+  (define oo/oc? (or (eq? method 'open-open) (eq? method 'open-closed)))
+  ;;bg; refactor into a match
+  (match (cons lo hi)
+   [(cons (neg-inf) (pos-inf))
+    (integrate-open-open new-integrand -pi/2 +pi/2 eps)]
+   [(cons (neg-inf) (neg-inf))
+    0.0]
+   [(cons (neg-inf) _)
+    (if oo/co? ;;bg; is this a scmutils typo?
+        (integrate-open-open new-integrand -pi/2 (atan hi) eps)
+        (integrate-open-closed new-integrant -pi/2 (atan hi) eps))]
+   [(cons (pos-inf) (pos-inf))
+    0.0]
+   [(cons (pos-inf) (neg-inf))
+    (- (integrate-open-open new-integrand -pi/2 +pi/2 eps))]
+   [(cons (pos-inf) _)
+    (if oo/oc?
+        (- (integrate-open-open new-integrand (atan hi) -pi/2 eps))
+        (- (integrate-closed-open new-integrand (atan hi) +pi/2 eps)))]
+   [(cons _ (pos-inf))
+    (if oo/oc?
+        (integrate-open-open new-integrand (atan lo) +pi/2 eps)
+        (integrate-closed-open new-integrand (atan lo) +pi/2 eps))]
+   [(cons _ (neg-inf))
+    (if oo/oc?
+        (- (integrate-open-open new-integrand -pi/2 (atan lo) eps))
+        (- (integrate-closed-open new-integrand -pi/2 (atan lo) eps)))]
+   [_ (error 'improper-integral (format "Bad bounds: expected -inf or +inf, but got lo=~a and hi=~a" lo hi))]))
+
+;; Simpler version from Press et~al.
+(define *improper-integral-breakpoint* (make-parameter 1.0))
+(define (evaluate-improper-integral/press method integrand hi lo eps)
+  (define (new-integrand theta)
+      (/ (integrand (/ 1.0 theta))
+         (square theta theta)))
+  (match (cons lo hi)
+   [(cons (neg-inf) (neg-inf))
+    0.0]
+   [(cons (neg-inf) (pos-inf))
+    (+ (integrate-closed-open new-integrand (/ -1.0 (*improper-integral-breakpoint*)) 0.0 eps)
+       (integrate-closed-closed integrand (- (*improper-integral-breakpoint*)) (*improper-integral-breakpoint*) eps)
+       (integrate-open-closed new-integrand 0.0 (/ 1.0 (*improper-integral-breakpoint*)) eps))]
+    [(cons (neg-inf) _)
+     (if (<= hi (- (*improper-integral-breakpoint*)))
+       (integrate-open-open new-integrand (/ -1.0 hi) 0.0 eps)
+       (+ (integrate-closed-open new-integrand (/ -1.0 (*improper-integral-breakpoint*)) 0.0 eps)
+          (integrate-closed-open integrand (- (*improper-integral-breakpoint*)) hi eps)))]
+   [(cons (pos-inf) (neg-inf))
+    (- (+ (integrate-closed-open new-integrand (/ -1.0 (*improper-integral-breakpoint*)) 0.0 eps)
+          (integrate-closed-closed integrand (- (*improper-integral-breakpoint*)) (*improper-integral-breakpoint*) eps)
+          (integrate-open-closed new-integrand 0.0 (/ 1.0 (*improper-integral-breakpoint*)) eps)))]
+   [(cons (pos-inf) (pos-inf))
+     0.0]
+   [(cons (pos-inf) _)
+    (if (>= hi (*improper-integral-breakpoint*))
+      (- (integrate-open-open new-integrand (/ 1.0 hi) 0.0 eps))
+      (- (+ (integrate-closed-open new-integrand (/ 1.0 (*improper-integral-breakpoint*)) 0.0 eps)
+            (integrate-closed-open integrand (*improper-integral-breakpoint*) hi eps))))]
+   [(cons _ (neg-inf))
+    (if (<= lo (- (*improper-integral-breakpoint*)))
+        (integrate-open-open new-integrand (/ -1.0 lo) 0.0 eps)
+        (+ (integrate-closed-open new-integrand (/ -1.0 (*improper-integral-breakpoint*)) 0.0 eps)
+           (integrate-closed-open integrand (*improper-integral-breakpoint*) lo eps)))]
+   [(cons _ (pos-inf))
+    (if (>= lo (*improper-integral-breakpoint*))
+        (integrate-open-open new-integrand 0.0 (/ 1.0 lo) eps)
+        (+ (integrate-open-closed integrand lo (*improper-integral-breakpoint*) eps)
+           (integrate-open-closed new-integrand 0.0 (/ 1.0 (*improper-integral-breakpoint*)) eps)))]
+   [_ (error 'improper-integral/press (format "Bad bounds: expect -inf or +inf, got lo=~a and hi=~a" lo hi))]))
+
+;;bg; idk...
 ;(define (bulirsch-stoer-quadrature f t1 t2 eps)
 ;  ((advance-generator
 ;    (bulirsch-stoer-lisptran
@@ -205,9 +181,9 @@
 ;    (- t2 t1)
 ;    (lambda (ns dt h cont) (cont))
 ;    (lambda (ns dt sdt) (vector-ref ns 1))))
-;
-;;; =============================================================================
-;
+
+;; =============================================================================
+
 ;(module+ test
 ;  (require rackunit)
 ;
