d97ed504d3889e1dd0305f3dd1dc8a1f5e508d44
ben <types@ccs.neu.edu>
2015-10-08 15:20:54 -0400

    [infra] enable travis-ci and coveralls

--- info.rkt
+++ info.rkt
@@ -6,7 +6,13 @@
 
 (define collection "mechanics")
 (define deps '("base"))
-;(define build-deps '("scribble-lib" "racket-docs"))
+(define build-deps
+  '("cover"
+    "cover-coveralls"
+    "racket-doc"
+    "rackunit-lib"
+    "scribble-lib"
+    ))
 (define pkg-desc "Library for SICM")
 (define pkg-authors '(ben ben))
 (define version "0.0")
