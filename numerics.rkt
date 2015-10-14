797b7b19a0052adbe3638a17cdbb5b7ed0083563
bkc39 <bkc39@cornell.edu>
2015-10-13 21:46:10 -0500

    [#18] Finish wrapper for numerics/fuctions

--- numerics.rkt
+++ numerics.rkt
@@ -1,12 +1,26 @@
 #lang racket/base
 
-;; TODO import the rest of numerics/functions
-;;      rename conflicting identifiers (between elliptic & elliptic-flo, etc)
+(require
+ mechanics/private/numerics/functions/bessel
+ mechanics/private/numerics/functions/bessjs
+ mechanics/private/numerics/functions/elliptic
+ (prefix-in flo: mechanics/private/numerics/functions/elliptic-flo))
 
 (provide
-  (all-from-out mechanics/private/numerics/functions/bessel)
-)
-
-(require
-  mechanics/private/numerics/functions/bessel
-)
+ (all-from-out mechanics/private/numerics/functions/bessel)
+ (all-from-out mechanics/private/numerics/functions/bessjs)
+ 
+ (rename-out
+  [bessj₀ bessj0]
+  [bessj₁ bessj1]
+  [bessy₀ bessy0]
+  [bessy₁ bessy1]
+  [bessh₀ bessh0]
+  [bessh₁ bessh1])
+ 
+ (rename-out
+  [Rf Carlson-elliptic₁]
+  [Rf Carlson-elliptic-1]
+  [Carlson-elliptic₁-simple Carlson-elliptic-1-simple]
+  [Rd Carlson-elliptic₂]
+  [Rd Carlson-elliptic-2]))
