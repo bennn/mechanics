6ad400c59348a4748b4f08506fd15b762ffe1539
bkc39 <bkc39@cornell.edu>
2015-10-13 23:08:59 -0500

    [#18] Add consistent ascii names to numerics interface.

--- numerics.rkt
+++ numerics.rkt
@@ -12,15 +12,30 @@
  
  (rename-out
   [bessj₀ bessj0]
+  [bessj₀ bessj_0]
+
   [bessj₁ bessj1]
+  [bessj₁ bessj_1]
+  
   [bessy₀ bessy0]
+  [bessy₀ bessy_0]
+  
   [bessy₁ bessy1]
+  [bessy₁ bessy_1]
+  
   [bessh₀ bessh0]
-  [bessh₁ bessh1])
+  [bessh₀ bessh_0]
+  
+  [bessh₁ bessh1]
+  [bessh₁ bessh_1])
  
  (rename-out
   [Rf Carlson-elliptic₁]
   [Rf Carlson-elliptic-1]
+  [Rf Carlson-elliptic_1]
+  
   [Carlson-elliptic₁-simple Carlson-elliptic-1-simple]
+  
   [Rd Carlson-elliptic₂]
-  [Rd Carlson-elliptic-2]))
+  [Rd Carlson-elliptic-2]
+  [Rd Carlson-elliptic_2]))
